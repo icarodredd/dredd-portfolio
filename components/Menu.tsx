1bb5bbe086b89d0bf4433fc3a74fcf9752545044
icarodredd <icarog854@gmail.com>
2024-10-19 11:49:21 -0300

    add quotesmd project

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import pic from "@/public/pic.jpg";
 import hai from "@/public/hai.jpeg";
 import kodis from "@/public/kodis.jpeg";
+import quotes from "@/public/quotesmd.jpeg";
 import { Button } from "./ui/button";
 
 export default function Menu() {
@@ -9,12 +10,10 @@ export default function Menu() {
     <main className="mt-20">
       <h1 className="italic text-3xl text-white">Hello There!</h1>
       <p className="text-lg text-white mt-4 light">
-        Hey, this website tells a little story about me, professionally. You can
-        check out a few projects and my social media.
+        Hey, this website tells a little story about me, professionally. You can check out a few projects and my social
+        media.
       </p>
-      <p className="text-lg text-white mt-4">
-        {"You'll also find some random pictures that just make sense."}
-      </p>
+      <p className="text-lg text-white mt-4">{"You'll also find some random pictures that just make sense."}</p>
       <div className="flex justify-center mt-4">
         <Image
           className="rounded-lg outline outline-1 outline-offset-2 outline-gray-600"
@@ -23,18 +22,38 @@ export default function Menu() {
           alt="Picture of a pool in a beautiful green place"
         />
       </div>
-      <p className="text-lg text-white text-center p-2">
-        Juazeiro do Norte, CE, Brazil.
-      </p>
+      <p className="text-lg text-white text-center p-2">Juazeiro do Norte, CE, Brazil.</p>
       <h1 className="italic text-3xl text-white mt-20">Latest Projects</h1>
       <p className="text-lg text-white mt-4 light mb-8">
-        Here are some of my projects that I have done throughout my career as a
-        fullstack developer.
+        Here are some of my projects that I have done throughout my career as a fullstack developer.
       </p>
       <div className="flex justify-center mb-8">
         <div className="outline outline-1 outline-offset-2 outline-gray-600 rounded-lg">
           <div>
-            <Image src={hai} width={500} alt="Image of the HAI project" />
+            <Image
+              src={quotes}
+              width={500}
+              alt="Image of the quotes project"
+            />
+          </div>
+          <a href="https://quotes-md.vercel.app/">
+            <Button
+              className="text-white w-full rounded-none"
+              variant={"ghost"}
+            >
+              Visit
+            </Button>
+          </a>
+        </div>
+      </div>
+      <div className="flex justify-center mb-8">
+        <div className="outline outline-1 outline-offset-2 outline-gray-600 rounded-lg">
+          <div>
+            <Image
+              src={hai}
+              width={500}
+              alt="Image of the HAI project"
+            />
           </div>
           <a href="https://github.com/icarodredd/HumanizeAI">
             <Button
@@ -49,7 +68,11 @@ export default function Menu() {
       <div className="flex justify-center mb-8">
         <div className="outline outline-1 outline-offset-2 outline-gray-600 rounded-lg">
           <div>
-            <Image src={kodis} width={500} alt="Image of the HAI project" />
+            <Image
+              src={kodis}
+              width={500}
+              alt="Image of the HAI project"
+            />
           </div>
           <a href="https://kodis-nu.vercel.app/">
             <Button
